d0b449786eade114f22c51338c1838acc583b0f5
Pablo <pablo.0698@hotmail.com>
2020-12-12 15:57:25 -0500

    juego terminado

--- Assets/Scripts/UI/obj_ui_window.cs
+++ Assets/Scripts/UI/obj_ui_window.cs
@@ -1,5 +1,4 @@
-﻿using Pathfinding;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,49 +8,28 @@ public class obj_ui_window : MonoBehaviour
     public GameObject _window;
     public List<Sprite> _changeSprite;
 
-    private AIPath _path;
     private movement _movement;
-    private bool _UI;
 
     private void Awake()
     {
         _image = GetComponent<Image>();
         _image.sprite = _changeSprite[0];
 
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        _movement = player.GetComponent<movement>();
-        _path = player.GetComponent<AIPath>();
-
-        _UI = false;
+        _movement = GameObject.FindGameObjectWithTag("Player").GetComponent<movement>();
     }
-    public void Show_Window()
+    public void Window(bool active)
     {
-        if (_UI)
-        {
-            _window.SetActive(true);
-            _movement.enabled = _path.enabled = false;
-        }
+        _window.SetActive(active);
+        _movement.enabled = !active;
     }
-    public void Hide_Window()
-    {
-        _window.SetActive(false);
-        _movement.enabled = _path.enabled = true;
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && _window != null)
-        {
             _image.sprite = _changeSprite[1];
-            _UI = true;
-        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && _window != null)
-        {
             _image.sprite = _changeSprite[0];
-            _UI = false;
-        }
     }
 }
